bf93707ca7abf25d696bf43f0c6915e037c5f769
LongPG <gialongpbc16@gmail.com>
2024-01-26 16:48:01 +0700

    demo

---
+++ WindowsFormsApp1/WindowsFormsApp1/long.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class @long
+    {
+        int x = 1;
+        int y = 2;
+    }
+}
